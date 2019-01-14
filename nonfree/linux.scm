e384097d1144dad235ff3434bf3bc301557372bb
Marius Bakke <mbakke@fastmail.com>
2019-01-14 18:25:19 +0100

    Don't reference GNU Guix in copyright notice.

--- nonfree/linux.scm
+++ nonfree/linux.scm
@@ -7,20 +7,18 @@
 ;;; Copyright © 2015 Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
 ;;; Copyright © 2015, 2017 Andy Wingo <wingo@igalia.com>
 ;;;
-;;; This file is part of GNU Guix.
-;;;
-;;; GNU Guix is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation; either version 3 of the License, or (at
 ;;; your option) any later version.
 ;;;
-;;; GNU Guix is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (nonfree linux)
   #:use-module ((guix licenses) #:prefix license:)
