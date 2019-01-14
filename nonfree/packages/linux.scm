4bf6f5b7100448d93d408e2a0f4fbc0f5b595a25
Marius Bakke <mbakke@fastmail.com>
2019-01-14 19:44:59 +0100

    Keep package modules in (nonfree packages).
    This frees up the (nonfree) namespace for other things.

--- nonfree/packages/linux.scm
+++ nonfree/packages/linux.scm
@@ -21,7 +21,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(define-module (nonfree linux)
+(define-module (nonfree packages linux)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages tls)
